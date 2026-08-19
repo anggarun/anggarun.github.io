dd8452dcf5b388e321d79663a422d8918cb72560
Angga Nur Diansyah <anggarun8@gmail.com>
2026-08-19 20:42:18 +0700

    Jadikan Portofolio

---
+++ js/main.js
@@ -0,0 +1,81 @@
+(function ($) {
+    "use strict";
+    
+    // Smooth scrolling for all internal anchor links (with fallback easing)
+    $('a[href^="#"]').on('click', function (event) {
+        var targetHash = this.hash;
+        if (targetHash && $(targetHash).length) {
+            event.preventDefault();
+            var easingMethod = typeof $.easing !== 'undefined' && $.easing.easeInOutExpo ? 'easeInOutExpo' : 'swing';
+            $('html, body').animate({
+                scrollTop: $(targetHash).offset().top - 30
+            }, 800, easingMethod);
+
+            // update active class for navbar items if applicable
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+    
+
+    // Typed Initiate
+    if ($('.header h2').length == 1) {
+        var typed_strings = $('.header .typed-text').text();
+        var typed = new Typed('.header h2', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+    
+    
+    // Skills
+    $('.skills').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+    
+    
+    // Porfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+    
+    // Review slider
+    $('.review-slider').slick({
+        autoplay: true,
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+})(jQuery);
+
